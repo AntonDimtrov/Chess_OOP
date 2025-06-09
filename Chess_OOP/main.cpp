dca1a3bef7bbcbe405950d3af382871046a02c08
Anton Dimitrov <antonjudo680@gmail.com>
2025-06-09 16:33:28 +0300

    Initial Commit

---
+++ Chess_OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess_OOP
+//
+//  Created by Anton Dimitrov on 9.06.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
